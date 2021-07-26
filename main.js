ef5dcd5389f1afcadf78577bf58a6c082540fdf1
Matusz27 <mateusz.wozniak2000@gmail.com>
2021-07-26 17:33:24 +0200

    Finished animations the page is now done how it was suppose to be

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+ScrollReveal({duration: 2000, distance: '200px', origin: 'top' });
+
+
+ScrollReveal().reveal('#muller')
+ScrollReveal().reveal('#drone')
+ScrollReveal().reveal('.info-text')
+ScrollReveal().reveal('.info-text h1')
+ScrollReveal().reveal('.faq-main h1')
+ScrollReveal().reveal('.faq-main h2')
+ScrollReveal().reveal('.faq-main vl')
+ScrollReveal().reveal('.faq-con')
+ScrollReveal().reveal('.contact-head')
+ScrollReveal().reveal('.contact-head h1')
+ScrollReveal().reveal('.contact-form', { origin: 'left' })
+ScrollReveal().reveal('.contact-info', { origin: 'right' })
